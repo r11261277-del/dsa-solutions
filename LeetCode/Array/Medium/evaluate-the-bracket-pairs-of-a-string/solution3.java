30cd825a8c1c527dd8528abc9fd73d897bb3d99a
r11261277-del <r11261277@gmail.com>
2026-09-26 20:12:41 +0530

    Add Evaluate the Bracket Pairs of a String - LeetCode (Medium) [LeetCode] - Solution 3 (Java)

---
+++ LeetCode/Array/Medium/evaluate-the-bracket-pairs-of-a-string/solution3.java
@@ -0,0 +1,23 @@
+        StringBuilder key = new StringBuilder();
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                addKey = true;
+            } else if (c == ')') {
+                if (dict.containsKey(key.toString())) {
+                    res.append(dict.get(key.toString()));
+                } else {
+                    res.append('?');
+                }
+                addKey = false;
+                key.setLength(0);
+            } else if (addKey) {
+                key.append(c);
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
